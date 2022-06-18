44a853f7cb9f1b8d395776100623c18b85790cfc
OkoyeCharles <okoyecharles509@gmail.com>
2022-06-18 18:48:34 +0100

    Add changes

--- app.js
+++ app.js
@@ -7,53 +7,56 @@ let object2img = document.getElementById('object2-img')
 let body = document.getElementById('body')
 let playerimg = document.getElementById('player-img')
 let score = document.getElementById('score')
-
+let gameover = document.getElementById('game-over')
+let gameoverscore = document.getElementById('game-over-score')
+let gameoverhighscore = document.getElementById('game-over-high-score')
+let scorenum = 0
 let getScore = () => {
-  let num = 0
+  
   setInterval(() => {
-    if (object1.offsetLeft + object1.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object1.offsetLeft){
-      num += 1
+    if (object1.offsetLeft + object1.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object1.offsetLeft) {
+      scorenum += 1
     }
   }, 20)
   setInterval(() => {
-    if (object2.offsetLeft + object2.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object2.offsetLeft){
-      num += 1
+    if (object2.offsetLeft + object2.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object2.offsetLeft) {
+      scorenum += 1
     }
   }, 100)
-  setInterval(() => score.innerHTML = `SCORE: ${Math.ceil(num/10)}`, 30)
+  setInterval(() => score.innerHTML = `SCORE: ${Math.ceil(scorenum / 10)}`, 30)
 }
 getScore()
 
 const asteroids = ['./images/a1.png', './images/a2.png', './images/a3.png', './images/a4.png', './images/a5.png', './images/a6.png', './images/a7.png', './images/a8.png']
 
 const randomA = (img) => {
-  let imgnum = Math.floor(Math.random()*10)
+  let imgnum = Math.floor(Math.random() * 10)
   if (imgnum === 8) {
-    img.src = asteroids[imgnum -1];
+    img.src = asteroids[imgnum - 1];
   }
   else if (imgnum === 9) {
     img.src = asteroids[imgnum - 3];
   }
-  else {img.src = asteroids[imgnum];}
+  else { img.src = asteroids[imgnum]; }
 }
 
 const randomPos = () => {
-  let num = Math.floor(Math.random()*350)
+  let num = Math.floor(Math.random() * 350)
   if (num < 20) {
     return num += 20
-  } 
-  else {return num}
+  }
+  else { return num }
 }
 
 playerimg.src = "./images/ship.png"
 
-setInterval(() => {object1.style.bottom = randomPos().toString()+'px'; randomA(object1img)}, 2000);
-setInterval(() => {object2.style.bottom = randomPos().toString()+'px'; randomA(object2img)}, 2500);
+setInterval(() => { object1.style.bottom = randomPos().toString() + 'px'; randomA(object1img) }, 2000);
+setInterval(() => { object2.style.bottom = randomPos().toString() + 'px'; randomA(object2img) }, 2500);
 
 
 function checkKey(e) {
   e = e || window.event;
-  
+
   if (e.keyCode == '38' && player.offsetTop > 0) {
     player.style.top = '0px'
   }
@@ -62,35 +65,55 @@ function checkKey(e) {
   }
 }
 
+setInterval(() => {
+  if (player.offsetTop === 0 || player.offsetTop === 360) {
+    gameover.classList.add('display')
+    player.classList.add('clear')
+    object1.id='done1'
+    object2.id='done2'
+    const gameoverscorenum = Math.ceil(scorenum / 10)
+    gameoverscore.innerHTML = `YOU SCORED ${gameoverscorenum}`
+    gameoverhighscore.innerHTML = `HIGH SCORE: ${parseInt(localStorage.getItem('high_score'))}`
+    setHighScore(gameoverscorenum)
+  }
+}, 10)
 
 setInterval(() => {
-  if (object1.offsetTop < player.offsetTop + player.offsetHeight && object1.offsetTop + object1.offsetHeight > player.offsetTop && object1.offsetLeft + object1.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object1.offsetLeft){
-    alert('game over')
+  if (object1.offsetTop < player.offsetTop + player.offsetHeight && object1.offsetTop + object1.offsetHeight > player.offsetTop && object1.offsetLeft + object1.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object1.offsetLeft) {
+    gameover.classList.add('display')
+    player.classList.add('clear')
+    object1.id='done1'
+    object2.id='done2'
+    const gameoverscorenum = Math.ceil(scorenum / 10)
+    gameoverscore.innerHTML = `YOU SCORED ${gameoverscorenum}`
+    gameoverhighscore.innerHTML = `HIGH SCORE: ${parseInt(localStorage.getItem('high_score'))}`
+    setHighScore(gameoverscorenum)
   }
 }, 10)
 setInterval(() => {
-  if (object2.offsetTop < player.offsetTop + player.offsetHeight && object2.offsetTop + object2.offsetHeight > player.offsetTop && object2.offsetLeft + object2.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object2.offsetLeft){
-    alert('game over')
+  if (object2.offsetTop < player.offsetTop + player.offsetHeight && object2.offsetTop + object2.offsetHeight > player.offsetTop && object2.offsetLeft + object2.offsetWidth > player.offsetWidth && player.offsetLeft + player.offsetWidth > object2.offsetLeft) {
+    gameover.classList.add('display')
+    player.classList.add('clear')
+    object1.id='done1'
+    object2.id='done2'
+    const gameoverscorenum = Math.ceil(scorenum / 10)
+    gameoverscore.innerHTML = `YOU SCORED ${gameoverscorenum}`
+    gameoverhighscore.innerHTML = `HIGH SCORE: ${parseInt(localStorage.getItem('high_score'))}`
+    setHighScore(gameoverscorenum)
   }
 }, 10)
+
+let setHighScore = (num) => {
+  if (!localStorage.getItem('high_score')){
+    localStorage.setItem('high_score', JSON.parse(num))
+  }
+  else if (parseInt(localStorage.getItem('high_score')) <= num) {
+    localStorage.setItem('high_score', JSON.parse(num))
+  }
+  return
+}
 
 
 window.addEventListener('keydown', (e) => {
   checkKey(e)
 })
-
-// const checkCollision = (player, obj) => {
-//   var p = player.getBoundingClientRect();
-//   setInterval(() => {let o = obj.getBoundingClientRect();}, 50)
-//   console.log(o.left)
-// }
-
-// checkCollision(playerimg, object1img)
-
-  // if (p.left < obj.left + obj.width && p.left + p.width > obj.left && p.top < obj.top + obj.height && p.top + p.height > obj.top) {
-  //   console.log('now')
-  // }
-  // else {
-  //   console.log('later')
-  // }
-
